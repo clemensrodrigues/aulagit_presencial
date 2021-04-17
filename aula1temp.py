95a63643563f9058f6b835c32e93896cc03acd70
Clemens Rodrigues <clemens05@msn.com>
2021-04-17 11:57:19 -0300

    aula1temp

---
+++ aula1temp.py
@@ -0,0 +1,9 @@
+
+
+tempFahrenheit = float (input("digite a temperatura em fahrenheit:"))
+
+sub1 = float(tempFahrenheit-32)
+sub2 = float(5/9)
+tempCelsius = float(sub1*sub2)
+
+print("tempCelsius:{:.2f}".format(tempCelsius))
